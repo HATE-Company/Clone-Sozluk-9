77c22673a536f2947a1dcef895ae511ab906e2e2
Merve Şimşek <93996187+Merve-Simsek92@users.noreply.github.com>
2023-01-15 10:43:25 +0300

    dropdown çalışması

--- clon-sozluk/src/App.js
+++ clon-sozluk/src/App.js
@@ -1,25 +1,37 @@
-import './App.css';
-import logo from "./helpers/logo.svg"
+import "./App.css";
+import logo from "./helpers/logo.svg";
+import Dropdown from "react-bootstrap/Dropdown";
 function App() {
   return (
- 
     <div className="App">
-   <div className="frame1">
-      <div className="frame1-rectangle1">
-<div className="frame1-reactangele1-logo">
-<img src={logo}  />
-</div>
-<div className="frame1-rectangle1-search">
-  <div className="frame1-rectangle1-search-vector">
+      <div className="frame1">
+        <div className="frame1-rectangle1">
+          <div className="frame1-reactangele1-logo">
+            <img src={logo} />
+          </div>
+          <div className="frame1-rectangle1-rectangle2"></div>
+          <div className="frame1-rectangle1-search-text">Search</div>
+          <div className="frame1-rectangle1-group1">
+            <div className="frame1-rectangle1-group1-vector"></div>
+            <div className="frame1-rectangle1-group1-vector-circle"></div>
+          </div>
+          <div className="frame1-rectangle1-dropdown">
+          <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
 
-  </div>
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
 
-</div>
+          </div>
+        </div>
       </div>
-      </div>
-
     </div>
-
   );
 }
 
