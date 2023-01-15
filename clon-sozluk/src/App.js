b832e2178586c2d90055f129e890b19bdd28b8b1
Merve Şimşek <93996187+Merve-Simsek92@users.noreply.github.com>
2023-01-15 09:28:10 +0300

    addLogo

---
+++ clon-sozluk/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import logo from "./helpers/logo.svg"
+function App() {
+  return (
+ 
+    <div className="App">
+   <div className="frame1">
+      <div className="frame1-rectangle1">
+<div className="frame1-reactangele1-logo">
+<img src={logo}  />
+</div>
+<div className="frame1-rectangle1-search">
+  <div className="frame1-rectangle1-search-vector">
+
+  </div>
+
+</div>
+      </div>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default App;
