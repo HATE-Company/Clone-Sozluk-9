f25d8ed3dc4abc1fe848ccd4b879a50e254c5800
Merve Şimşek <93996187+Merve-Simsek92@users.noreply.github.com>
2023-01-16 21:36:27 +0300

    icons

--- clon-sozluk/src/App.js
+++ clon-sozluk/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import logo from "./helpers/logo.svg";
 import Dropdown from "react-bootstrap/Dropdown";
+import {  BsPerson} from "react-icons/bs";
+import {game}  from "./game.svg";
 function App() {
   return (
     <div className="App">
@@ -16,19 +18,68 @@ function App() {
             <div className="frame1-rectangle1-group1-vector-circle"></div>
           </div>
           <div className="frame1-rectangle1-dropdown">
-          <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
+          <Dropdown  className="drop" >
+      <Dropdown.Toggle >
+     <BsPerson className="drop"/>
       </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
+     
     </Dropdown>
 
           </div>
+          <div className="frame1-rectangle1-rectangle54">
+            
+
+          </div>
+          <div className="frame1-rectangle1-topics">
+  Topics
+          </div>
+          <div className="frame1-rectangle1-game-vector">
+       <game/>
+          </div>
+          <div className="frame1-rectangle1-game-vector-text">
+        Gaming
+          </div>
+          <div className="frame1-rectangle1-sport-vector-text">
+        Sports
+          </div>
+          <div className="frame1-rectangle1-sport-vector">
+        
+          </div>
+
+          <div className="frame1-rectangle1-television-vector-text">
+        Television
+          </div>
+          <div className="frame1-rectangle1-television-vector">
+            
+        
+          </div>
+          <div className="frame1-rectangle1-celebrity-vector">
+            
+        
+            </div>
+            <div className="frame1-rectangle1-celebrity-vector-text">
+            Celebrity
+        
+            </div>
+            <div className="frame1-rectangle1-business-vector">
+            
+        
+            </div>
+            <div className="frame1-rectangle1-business-vector-text">
+          Business
+        
+            </div>
+            <div className="frame1-rectangle1-more-vector">
+            
+        
+            </div>
+            <div className="frame1-rectangle1-more-vector-text">
+         More
+        
+            </div>
+          
+
         </div>
       </div>
     </div>
